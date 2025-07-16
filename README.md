539659cd3404ff9c8764987872f265231200e79b
EnterpriseGoose <58440950+EnterpriseGoose@users.noreply.github.com>
2025-07-16 10:56:20 -0400

    Initial website

---
+++ README.md
@@ -0,0 +1,7 @@
+# overflow: hidden;
+
+## a ysws about not using scroll
+
+This is a ysws where if you ship a website that doesn't have any native scrollbars, you can get some stickers + some flex tape!!
+
+For more info, head to #overflow-hidden on slack.
---
+++ src/entry-server.tsx
@@ -0,0 +1,35 @@
+// @refresh reload
+import { createHandler, StartServer } from '@solidjs/start/server';
+
+export default createHandler(() => (
+  <StartServer
+    document={({ assets, children, scripts }) => (
+      <html lang="en">
+        <head>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <link
+            rel="preconnect"
+            href="https://fonts.googleapis.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Aleo:ital,wght@0,100..900;1,100..900&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="icon" href="/favicon.ico" />
+          {assets}
+        </head>
+        <body>
+          <div id="app">{children}</div>
+          {scripts}
+        </body>
+      </html>
+    )}
+  />
+));
